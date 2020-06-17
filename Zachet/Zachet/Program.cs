ba406c0e15f745d0c79f309843d434fd032fe5bb
Ivan Veselkov <nerorus97@gmail.com>
2020-06-17 19:54:36 +0400

    Зачетное задание
    Реализация быстрой и пирамидальной сортировок

---
+++ Zachet/Zachet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zachet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickSort t = new QuickSort();
+
+            HeapSort h = new HeapSort();
+            int[] array = new int[] {1,5,2,5,23,7,9,5,1};
+
+            //h.Sort(array);
+            //t.QuickSort(array, 0, array.Length-1);
+
+            foreach (int i in array)
+            {
+                Console.Write(i + "  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Zachet/Zachet/QuickSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zachet
+{
+    class QuickSort
+    {
+        public void Sort(int[] array, int first, int last)
+        {
+            int mid, count;
+            int f = first, l = last;
+            mid = array[(f + l) / 2];
+            do
+            {
+                while (array[f] < mid)
+                    f++;
+                while (array[l] > mid)
+                    l--;
+                if (f <= l)
+                {
+                    count = array[f];
+                    array[f] = array[l];
+                    array[l] = count;
+                    f++;
+                    l--;
+                }
+            } while (f < l);
+            if (first < l)
+                Sort(array, first, l);
+            if (f < last)
+                Sort(array, f, last);
+
+        }
+    }
+}
